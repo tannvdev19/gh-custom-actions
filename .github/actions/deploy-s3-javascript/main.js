c3c401d0eb0f963c1966859163203941a18f8ece
tannv-devv <tannvde31@gmail.com>
2023-07-21 15:10:43 +0700

    wgwge

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -3,7 +3,17 @@ const github = require('@actions/github');
 const exec = require('@actions/exec');
 
 function run() {
-    core.notice('Hello form my custom JavaScript action!')
+    // 1) Get some input values
+    const bucket = core.getInput('bucket', { required: true });
+    const bucketRegion = core.getInput('bucket-region', { required: true });
+    const distFolder = core.getInput('dist-folder', { required: true });
+
+    
+    // 2) Upload files
+    const s3Uri = `s3://${bucket}`;
+    exec.exec(`aws s3 async ${distFolder} ${s3Uri} --region ${bucketRegion}`);
+
+    core.notice('Hello form my custom JavaScript action!');
 }
 
 run();
