30974abee545c5376e36fec0d59d466649b06870
tannv-devv <tannvde31@gmail.com>
2023-07-21 15:35:10 +0700

    ggr

--- .github/actions/deploy-s3-javascript/main.js
+++ .github/actions/deploy-s3-javascript/main.js
@@ -13,7 +13,9 @@ function run() {
     const s3Uri = `s3://${bucket}`;
     exec.exec(`aws s3 sync ${distFolder} ${s3Uri} --region ${bucketRegion}`);
 
-    core.notice('Hello form my custom JavaScript action!');
+    const websiteUrl = `http://${bucket}.s3-website-${bucketRegion}.amazonaws.com`;
+    core.setOutput('website-url', websiteUrl);
+    
 }
 
 run();
